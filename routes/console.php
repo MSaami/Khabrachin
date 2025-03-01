102bae67cfcd1678d568e853e25f20e61702b792
Mehrdad Saami <mehrdad.sami@digikala.com>
2025-03-02 02:06:53 +0330

    remove some usless files

--- routes/console.php
+++ routes/console.php
@@ -1,8 +1,20 @@
 <?php
 
+use App\Jobs\FetchNewsJob;
+use App\Services\FetchService\GuardianFetchService;
+use App\Services\FetchService\NewsFetchService;
 use Illuminate\Foundation\Inspiring;
 use Illuminate\Support\Facades\Artisan;
+use Illuminate\Support\Facades\Schedule;
 
 Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
 })->purpose('Display an inspiring quote');
+
+
+// $service = new NewsFetchService(new GuardianFetchService());
+// Schedule::job(new FetchNewsJob($service))->everyTenSeconds();
+
+
+// Schedule::command('news:fetch')
+//     ->everyTenMinutes();
